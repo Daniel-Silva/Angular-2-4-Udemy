d8ffa215087d894ee9e024c6f91a7eec88554e2d
Daniel Silva <dani3l.dxp@gmail.com>
2018-04-16 14:49:44 -0300

    Aula I - Criando e entendendo rotas e estrutura angular

---
+++ Angular2/Angular-Website/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {ContatoModule} from './contato/contato.module';
+import {NossaVisaoModule} from './nossa-visao/nossa-visao.module';
+import {QuemSomosModule} from './quem-somos/quem-somos.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ContatoModule,
+    NossaVisaoModule,
+    AppRoutingModule,
+    QuemSomosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
